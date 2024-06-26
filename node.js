c6484661187808477d6b62f02f3252fa6759d05a
suganth <112330202+Suganth01@users.noreply.github.com>
2024-06-26 22:54:13 +0530

    Update node.js

--- node.js
+++ node.js
@@ -35,7 +35,7 @@ app.post('/submit-reservation', (req, res) => {
         res.status(200).send('Reservation submitted successfully');
     });
 });
-const PORT = process.env.PORT || 3300;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server is running on port ${PORT}`);
 });
